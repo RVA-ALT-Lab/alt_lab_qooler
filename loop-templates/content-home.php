b93830e22ee3cc74b0b5309555ddc292738bfac8
Tom Woodward <twwoodward@me.com>
2020-04-09 13:24:50 -0400

    weather keeps changing

--- loop-templates/content-home.php
+++ loop-templates/content-home.php
@@ -13,6 +13,20 @@
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 	</header><!-- .entry-header -->
+	<div class="weather-row row">
+				<div class="col-md-4 heavy">
+					2020<br>
+					Qooler<br>
+					design<br>
+					competition<br>
+					for a cooler<br>
+					future | <span class="triangle-down"></span><br>
+					VCU Qatar<br>
+				</div>
+				<div class="col-md-8" id="weather-block">
+					<div id="how-hot"></div>
+				</div>
+		</div>
 		<!--ACF BLOCK LOOP-->
 		<?php
 
@@ -34,7 +48,7 @@
 			endif;
 
 			?>
-		<!--no mas block loop-->		
+		<!--no mas block loop-->
 
 	<div class="entry-content">
 	
