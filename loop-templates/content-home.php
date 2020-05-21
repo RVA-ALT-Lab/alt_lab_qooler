6920aa338bb6480bf9d4b06326de6fbbe4dba42f
Tom Woodward <twwoodward@me.com>
2020-05-21 13:09:11 -0400

    now the bars work again and don't mess all up

--- loop-templates/content-home.php
+++ loop-templates/content-home.php
@@ -28,8 +28,9 @@
 				</div>
 		</div>
 	<div class="container-fluid">
+		<!--ORANGE BAR START-->
 		<div class="row">
-			<div class="orange-bars col-md-8 offset-md-4">
+			<div class="orange-bars col-md-9 offset-md-3">
 				<div class="row">
 					<div class="col-md-6">
 						<h2 class="bar-highlight top">Behind the Idea: Qatar is
@@ -44,6 +45,19 @@
 				</div>
 			</div>
 		</div>
+    <!--orange bar end-->
+	<!--map zone start--> 
+	<div class="row map-zone">
+		<div class="col-md-6 weird-map">
+			<img class="fluid" src="" alt="A triangular map view of the Earth.">
+		</div>
+		<div class="col-md-6 map-text">
+			<p>This competition seeks to find solutions to the problems for humans that climate change creates in a desert/marine climate like Qatar.</p>
+			<p>Being outside – whether working or playing – needs to be a part of life. But heat and humidity are uncomfortable, but in some cases, it can be dangerous.</p>
+			<p><span class="qooler">Qooler</span> is a design competition that seeks solutions that enable a comfortable and enjoyable life outdoors despite increasing heat</p>
+		</div>
+	</div>
+	<!--map zone end--> 
 	</div>
 
 		<!--ACF BLOCK LOOP-->
