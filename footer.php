ca1c5deb5424d8d93caffa21278ab49f37eb72f9
Tom Woodward <twwoodward@me.com>
2020-07-24 13:41:19 -0400

    footer work

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer - top left") ) : ?><?php endif;?>
 				</div>
 				<div class="col-md-4 logo-bar right">					
-					<img src="<?php echo get_template_directory_uri() . '/imgs/dark_vcu_qatar.svg' ?>" class="fluid footer-q-logo" alt="VCU Arts Qatar logo.">
+					<a href="https://qatar.vcu.edu/"><img src="<?php echo get_template_directory_uri() . '/imgs/dark_vcu_qatar.svg' ?>" class="fluid footer-q-logo" alt="VCU Arts Qatar."></a>
 				</div>
 				<div class="col-md-8 offset-md-2 logo-bar">		
 					<div class="footer-bar"></div>	
@@ -28,7 +28,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer - far left") ) : ?><?php endif;?>
 				</div>						
 				<div class="col-md-4 footer-widget right col-md-3">
-					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer - far right") ) : ?><?php endif;?>
+					<div class="social">
+						<a href="https://www.facebook.com/VCUQatar">Facebook</a>
+						<a href="https://twitter.com/VCUQatar">Twitter</a>
+						<a href="https://www.instagram.com/vcuqatar">Instagram</a>
+					</div>
 				</div>						
 											
 		</div><!-- row end -->
