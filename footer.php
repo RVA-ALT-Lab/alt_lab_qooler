6bb452c1acbc12310379ac614c3bd1fdcb1448f1
Tom Woodward <twwoodward@me.com>
2020-07-24 11:18:23 -0400

    footer work

--- footer.php
+++ footer.php
@@ -11,36 +11,33 @@ $the_theme = wp_get_theme();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<?php get_sidebar( 'footerfull' ); ?>
+<?php //get_sidebar( 'footerfull' ); ?>
  
 
-<div class="wrapper" id="wrapper-footer">
-	<img src="<?php echo get_template_directory_uri() . '/imgs/art_q_logo.svg' ?>" class="fluid footer-q-logo" alt="VCU Arts Qatar logo.">
-	<footer class="<?php echo esc_attr( $container ); ?>">
+
 		
 		<div class="row" id="footer">
-
-
-							<div class="footer-widget col-md-3">
-								<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer - far left") ) : ?><?php endif;?>
-							</div>
-							<div class="footer-widget col-md-3">
-								<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer - medium left") ) : ?><?php endif;?>
-							</div>
-							<div class="footer-widget col-md-3">
-								<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer - medium right") ) : ?><?php endif;?>
-							</div>
-							<div class="footer-widget col-md-3">
-								<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer - far right") ) : ?><?php endif;?>
-							</div>	
+				<div class="col-md-8 offset-md-2 logo-bar">
+					<img src="<?php echo get_template_directory_uri() . '/imgs/dark_vcu_qatar.svg' ?>" class="fluid footer-q-logo" alt="VCU Arts Qatar logo.">
+				</div>			
+				
+				<div class="footer-widget col-md-3">
+					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer - far left") ) : ?><?php endif;?>
+				</div>
+				<div class="footer-widget col-md-3">
+					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer - medium left") ) : ?><?php endif;?>
+				</div>
+				<div class="footer-widget col-md-3">
+					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer - medium right") ) : ?><?php endif;?>
+				</div>
+				<div class="footer-widget col-md-3">
+					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer - far right") ) : ?><?php endif;?>
+				</div>	
 											
 
 
 		</div><!-- row end -->
 
-	</div><!-- container end -->
-
-</footer><!-- wrapper end -->
 
 </div><!-- #page we need this extra closing tag here -->
 
--- loop-templates/content-home.php
+++ loop-templates/content-home.php
@@ -253,13 +253,13 @@
 
 	<div class="entry-content">
 	
-		<?php the_content(); ?>
+		<?php //the_content(); ?>
 
 		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-			'after'  => '</div>',
-		) );
+		// wp_link_pages( array(
+		// 	'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+		// 	'after'  => '</div>',
+		// ) );
 		?>
 
 	</div><!-- .entry-content -->
