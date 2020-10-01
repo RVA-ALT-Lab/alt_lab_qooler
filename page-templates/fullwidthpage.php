b73c03f7be97f6c0add5c4347ce4c0ed79ffdfad
Tom Woodward <twwoodward@me.com>
2020-10-01 13:15:39 -0400

    make single full width page match home page

--- page-templates/fullwidthpage.php
+++ page-templates/fullwidthpage.php
@@ -13,11 +13,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="full-width-page-wrapper">
 
-	<div class="container" id="content">
+	<div class="container-fluid" id="content">
 
 		<div class="row">
 
-			<div class="col-md-12 content-area" id="primary">
+			<div class="col-md-8 offset-md-2 content-area" id="primary">
 
 				<main class="site-main" id="main" role="main">
 
