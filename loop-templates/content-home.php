09a7135e85babb5c9e940d21941dea800b687f08
woodwardtw <twwoodward@me.com>
2020-10-14 11:39:22 -0400

    home page updates

--- loop-templates/content-home.php
+++ loop-templates/content-home.php
@@ -14,9 +14,10 @@
 
 	</header><!-- .entry-header -->
 	<div class="weather-row row">
-				<div class="col-md-8 heavy">					
+				<div class="col-md-7 heavy">					
 				</div>
-				<div class="col-md-4" id="weather-block">
+				<div class="col-md-5">
+					<div id="weather-block"></div>
 					<div id="conditions">Current conditions in Doha, Qatar</div>
 					<div id="how-hot"></div>
 				</div>
