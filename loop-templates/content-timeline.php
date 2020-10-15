9bd80f24fd9250730ab0860e8b9985fefc83f027
woodwardtw <twwoodward@me.com>
2020-10-15 12:42:00 -0400

    ugly but funtional ugly loop

--- loop-templates/content-timeline.php
+++ loop-templates/content-timeline.php
@@ -14,10 +14,16 @@
     		$connector = '<div class="rainbow-connection c-box"></div>';
     	} else {
     		$connector = '';
-    	};?>
+    	};
+    	$ugly = 'plain';
+    	if(get_row_index()== 4){
+    		$ugly = 'ugly';
+    	}
+
+    	?>
       <?php echo $connector;?>
       <div class="c-box connect">
-				<div class="date-box plain">
+				<div class="date-box <?php echo $ugly;?> ">
 					<?php the_sub_field('timeline_text')?>
 					<div class="time">
 						<?php the_sub_field('timeline_date')?>
